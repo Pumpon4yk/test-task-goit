4bb53f70f79671a80f4e583b5e923ec490387705
Pumpon4yk <tt10392@gmail.com>
2023-04-21 22:18:30 +0200

    seting vite v1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  base: "./",
+  server: {
+    open: './index.html',
+    port: 8888,
+  },
+  preview: {
+    open: './index.html',
+    port: 8080,
+  },
 })
