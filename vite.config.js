75e74a9f92f85dd22ea2e413be4bb8e298d43040
Pumpon4yk <tt10392@gmail.com>
2023-04-21 20:29:04 +0200

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      base:  "https://Pumpon4yk.github.io/test-task-goit/"
-    },
-  },
+  base: "/test-task-goit"
 })
