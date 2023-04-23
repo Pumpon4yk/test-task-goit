cc5271ac83ad4a87de2cdf110ef18fec8dc9c8bb
Pumpon4yk <tt10392@gmail.com>
2023-04-23 18:07:19 +0200

    test-vite

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
   base: "/test-task-goit/",
-  server: {
-//     open: './',
-    port: 8888,
-  },
-  preview: {
-//     open: './',
-    port: 8080,
-  },
+  plugins: [react()],
+  // server: {
+  //   open: './',
+  //   port: 8888,
+  // },
+  // preview: {
+  //   open: './',
+  //   port: 8080,
+  // },
 })
